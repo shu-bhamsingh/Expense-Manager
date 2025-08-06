254bb72f73e4e367bf5b2ff4ddcb474bd4daf045
Shubham Singh <shubhamsingh.s2003@gmail.com>
2025-08-06 15:00:00 +0530

    feat(backend): add user authentication with JWT

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const cors = require('cors');
 
+const SECRET = process.env.SECRET;
 const URL = process.env.URL;
+
+const User = require('./models/UserSchema');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 const PORT = 5001 || process.env.PORT;
 
@@ -14,12 +22,30 @@ app.get("/", (req, res) => {
     res.send('Hello world');
 });
 
+app.use('/auth', authRoutes);
+
 mongoose.connect(URL).then(() => {
     console.log('Connected to the database');
 }).catch((err) => {
     console.log(err);
     console.log('Error connecting to the database');
 });
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET;
+
+passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findOne({ _id: jwt_payload.identifier }).then(function (user) {
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    }).catch((err) => {
+        return done(err, false);
+    });
+}));
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
