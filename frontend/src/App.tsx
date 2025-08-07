f84f5b18c2fa049392ab5ae4c80eb6417c923616
Shubham Singh <shubhamsingh.s2003@gmail.com>
2025-08-07 11:45:00 +0530

    feat(frontend): initialize react app with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Router from "./routes";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      <Router />
+    </>
+  );
+};
+
+export default App;
